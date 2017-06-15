9e577c89399d32f30d49bb04cffc0100f26f5572
SachinKasar <kasarsac@WL308554.ap.tieto.com>
2017-06-15 16:39:29 +0530

    ReactJS-initial checkins.

---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    render
+}
+from "react-dom";
+
+class App extends React.Component {
+
+    render() {
+
+        return ( < div >
+            < h1 > Hello 123!!! < /h1>   < /div >
+        );
+    }
+
+}
+
+render( < App / > , document.getElementById("app"));
