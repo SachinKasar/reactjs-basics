c683ea77618e3d9d5e6358a8c9aab103f9c0b902
SachinKasar <kasarsac@WL308554.ap.tieto.com>
2017-06-15 17:03:15 +0530

    index js update.

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,14 @@
 import React from "react";
-import {
-    render
-}
-from "react-dom";
+import {render} from "react-dom";
 
 class App extends React.Component {
-
     render() {
-
-        return ( < div >
-            < h1 > Hello 123!!! < /h1>   < /div >
+        return ( 
+		<div>
+            <h1> Hello 123!!! </h1>
+    	</div>
         );
     }
-
 }
 
 render( < App / > , document.getElementById("app"));
